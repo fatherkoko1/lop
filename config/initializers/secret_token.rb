a2a8410729714fb2b5996f086c69efaf532a4022
Ibimina Koko <kokoibimina@gmail.com>
2012-08-11 02:55:03 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lop::Application.config.secret_token = '057bf64cc336699882ab2bfcb75a94fda091339160f9d4d010a02818d096dddfbdb3b7f40fe7a9153551586801dd1e09a96d7a8e28f63606894bb260bdbaef84'
